fe7552f86359d334a922fe7bfe3478b9780430a0
jango-fx <felix.groll@gmail.com>
2021-05-06 01:50:40 +0200

    fixed RefreshTimeline script
    TODO:
     - refactor asmdef's

--- Editor/RefreshTimelineWindow.cs
+++ Editor/RefreshTimelineWindow.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 using UnityEditor;
 using UnityEditor.Timeline;
 
+[ExecuteInEditMode]
 public class RefreshTimelineWindow : MonoBehaviour
 {
+    #if UNITY_EDITOR
     static public void Refresh()
     {
         TimelineEditorWindow thewindow = (TimelineEditorWindow)EditorWindow.GetWindow(typeof(TimelineEditorWindow), true, "Timeline", true);
         thewindow.Focus();
         thewindow.Repaint();
     }
+    #endif
 }
