76e87bc432adf5f04aafb027b2c5dcaa968b735f
jango-fx <felix.groll@gmail.com>
2021-05-06 01:52:35 +0200

    updated GameEventMarker
    see ScriptableObjects #0ec6c1147b5401ed18576934438456a446076ea7

---
+++ GameEventMarker/GameEventMarker.cs
@@ -0,0 +1,46 @@
+using System;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+//using ArtCom.ScriptableObjects.Events;
+
+namespace ArtCom.TimelineUtilities
+{
+    [Serializable, DisplayName("GameEvent Marker")]
+    public class GameEventMarker : Marker, INotification, INotificationOptionProvider
+    {
+        //[SerializeField] public bool retroactive;
+        //[SerializeField] public bool emitOnce;
+        [SerializeField] public bool emitInEditor;
+
+        //public string msg;
+        public ScriptableObject gameEvent;
+
+        // public PropertyName id { get { return new PropertyName(); } }
+
+        /*
+                NotificationFlags INotificationOptionProvider.flags
+                {
+                    get
+                    {
+                        return 
+                            (retroactive ? NotificationFlags.Retroactive : default(NotificationFlags)) |
+                            (emitOnce ? NotificationFlags.TriggerOnce : default(NotificationFlags)) |
+                            (emitInEditor ? NotificationFlags.TriggerInEditMode : default(NotificationFlags)); 
+                    }
+                }
+                */
+
+        public PropertyName id { get; }
+
+        NotificationFlags INotificationOptionProvider.flags
+        {
+            get
+            {
+                return
+                    (emitInEditor ? NotificationFlags.TriggerInEditMode : default(NotificationFlags));
+            }
+        }
+    }
+}
